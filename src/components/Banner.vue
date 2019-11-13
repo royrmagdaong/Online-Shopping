a7c3959b2cb8d3220b46844dd5ec9864a1a55a59
Roy <43959173+royrmagdaong@users.noreply.github.com>
2019-11-13 12:02:54 +0800

    Update Banner.vue

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -20,7 +20,7 @@
           align="center"
           justify="center"
         >
-          <div class="display-3">{{ slide }} PROMOS in NOVEMBER</div>
+          <div class="display-3">{{ slide }} PROMOS IN NOVEMBER</div>
         </v-row>
       </v-sheet>
     </v-carousel-item>
